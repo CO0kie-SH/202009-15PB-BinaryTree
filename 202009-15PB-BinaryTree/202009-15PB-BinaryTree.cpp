c0249934b86038e6903c024f791726090d6b2272
mhws2 <mhws2@MHWS2>
2020-09-16 21:57:37 +0800

    更新二叉树查找模板

--- 202009-15PB-BinaryTree/202009-15PB-BinaryTree.cpp
+++ 202009-15PB-BinaryTree/202009-15PB-BinaryTree.cpp
@@ -3,6 +3,8 @@
 
 //#include <iostream>
 #include "SearchTree.h"
+#include "CTbstree.h"
+
 void 二叉查找树()
 {
     SearchTree tree;
@@ -22,8 +24,29 @@ void 二叉查找树()
     tree.Back();	//后序遍历，左右根
 }
 
+void 二叉查找树模板()
+{
+    CTbstree<long> tree;
+    tree.Insert(0x0F);
+    tree.Insert(22);
+    tree.Insert(11);
+    tree.Print();
+    tree.Insert(9);
+    tree.Print();
+    //层序遍历
+    tree.PrintByDeep();
+    //前序遍历 根左右
+    tree.Front();	//前序遍历，根左右
+    tree.Middle();	//中序遍历，左根右
+    tree.Back();	//后序遍历，左右根
+    tree.Insert(2);
+    tree.Back();	//后序遍历，左右根
+
+}
+
 int main()
 {
     std::cout << "Hello World!\n";
     二叉查找树();
+    二叉查找树模板();
 }
--- 202009-15PB-BinaryTree/CTbstree.h
+++ 202009-15PB-BinaryTree/CTbstree.h
@@ -1,19 +1,57 @@
-#include "SearchTree.h"
+#pragma once
+#include <iostream>
+using namespace std;
 #include <vector>
 #include <cmath>
 
 
-SearchTree::SearchTree()
+template <class T>
+class CTbstree
 {
-}
+public:
+	// 定义结构体
+	typedef struct _NODE
+	{
+		T data;
+		//定义左右子树
+		_NODE* left;
+		_NODE* right;
+	}NODE, * PNODE;
+private:
+	// 定义根节点
+	PNODE m_root = nullptr;
+	unsigned int m_deep = 1;
+	void insert(PNODE& node, T data);
+	// 从二叉树内删除元素
+	void erase(PNODE& node, T data);
+	void print(PNODE& node, int w, char c);
 
-SearchTree::~SearchTree()
-{
-}
+	// 查找指定树的最小节点
+	//int min(PNODE& node);
+
+	// 查找指定树的最大节点
+	int max(PNODE& node);
+	void front(PNODE& node);	//前序遍历，根左右
+	void middle(PNODE& node);	//中序遍历，左根右
+	void back(PNODE& node, int& max);		//后序遍历，左右根
+public:
+	void Insert(T data);
+	// 接口: 从树中删除元素
+	void erase(T data);
+	void PrintByDeep();
+	void Print();
+
+	void Front();
+	void Middle();
+	void Back();
+};
+
 
 #pragma region 对内函数
+
+template <class T>
 // 内部插入函数
-void SearchTree::insert(PNODE& node, int data)
+void CTbstree<T>::insert(PNODE& node, T data)
 {
 	if (node == nullptr) {
 		node = (PNODE)new NODE{ data,nullptr,nullptr };
@@ -33,7 +71,8 @@ void SearchTree::insert(PNODE& node, int data)
 	}
 }
 
-void SearchTree::erase(PNODE& node, int data)
+template <class T>
+void CTbstree<T>::erase(PNODE& node, T data)
 {
 	//循环为空时弹出
 	if (node == nullptr) {
@@ -67,8 +106,8 @@ void SearchTree::erase(PNODE& node, int data)
 		}
 	}
 }
-
-void SearchTree::print(PNODE& node, int w, char c)
+template <class T>
+void CTbstree<T>::print(PNODE& node, int w, char c)
 {
 	if (node != nullptr)
 	{
@@ -77,15 +116,16 @@ void SearchTree::print(PNODE& node, int w, char c)
 		print(node->left, w + 4, '\\');
 	}
 }
-
-int SearchTree::max(PNODE& node)
+template <class T>
+int CTbstree<T>::max(PNODE& node)
 {
 	while (node->right)
 		node = node->right;
 	return node->data;
 }
 
-void SearchTree::front(PNODE& node)
+template <class T>
+void CTbstree<T>::front(PNODE& node)
 {
 	if (node) {
 		printf_s("%d ", node->data);
@@ -94,7 +134,8 @@ void SearchTree::front(PNODE& node)
 	}
 }
 
-void SearchTree::middle(PNODE& node)
+template <class T>
+void CTbstree<T>::middle(PNODE& node)
 {
 	if (node) {
 		middle(node->left);
@@ -102,8 +143,8 @@ void SearchTree::middle(PNODE& node)
 		middle(node->right);
 	}
 }
-
-void SearchTree::back(PNODE& node,int& max)
+template <class T>
+void CTbstree<T>::back(PNODE& node, int& max)
 {
 	if (node) {
 		if (node->left) ++max;
@@ -117,29 +158,31 @@ void SearchTree::back(PNODE& node,int& max)
 #pragma endregion
 
 #pragma region 对外接口
-
+template <class T>
 // 插入接口
-void SearchTree::Insert(int data)
+void CTbstree<T>::Insert(T data)
 {
 	insert(m_root, data);
 }
 
+template <class T>
 // 接口: 从树中删除元素
-void SearchTree::erase(int data)
+void CTbstree<T>::erase(T data)
 {
 	erase(m_root, data);
 }
 
-void SearchTree::PrintByDeep()
+template <class T>
+void CTbstree<T>::PrintByDeep()
 {
-	vector<PNODE> allptr = {m_root,m_root->left,m_root->right };
+	vector<PNODE> allptr = { m_root,m_root->left,m_root->right };
 	unsigned int deep = 1, index = 1, rig = 0;
 	bool ret = false;
-	while (deep<5)
+	while (deep < 5)
 	{
 		ret = true;
 		rig = index + (int)pow(2, deep);
-		printf_s("第%d层 %d~%d\t", deep+1, index, rig-1);
+		printf_s("第%d层 %d~%d\t", deep + 1, index, rig - 1);
 		for (unsigned int i = index; i < rig; i++)
 		{
 			printf_s("[%d]", i);
@@ -170,27 +213,33 @@ void SearchTree::PrintByDeep()
 	cout << "——————" << endl;
 }
 
+template <class T>
 //打印接口
-void SearchTree::Print()
+void CTbstree<T>::Print()
 {
 	print(m_root, 0, '*');
-	cout << "——————"<<endl;
+	cout << "——————" << endl;
 }
-void SearchTree::Front()
+
+template <class T>
+void CTbstree<T>::Front()
 {
 	front(m_root);
 	cout << "\n——————" << endl;
 }
-void SearchTree::Middle()
+
+template <class T>
+void CTbstree<T>::Middle()
 {
 	middle(m_root);
 	cout << "\n——————" << endl;
 }
-void SearchTree::Back()
+
+template <class T>
+void CTbstree<T>::Back()
 {
-	int max = 1;
+	int max = 0;
 	back(m_root, max);
-	cout << "\n——————最大层=" << max - 1 << endl;
+	cout << "\n——————最大层=" << max << endl;
 }
 #pragma endregion
-
